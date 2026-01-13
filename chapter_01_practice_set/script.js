4d6125f22e4eb55729f76104f5f92e20de7c81a5
abdulrahmaannhh <abdulrahmaannhh@gmail.com>
2026-01-13 15:44:06 +0530

    practice set 01 completel

---
+++ chapter_01_practice_set/script.js
@@ -0,0 +1,28 @@
+//Question 01
+let a = "AR"
+let b = 6
+console.log(a + b)
+
+// Question 02
+console.log(typeof (a + b))
+
+// Question 03
+const a1 = {
+    name: "AR",
+    "roll no.": "01",
+    batch: "06"
+}
+
+// a1="Abdul"
+console.log(a1)
+
+
+// Question 04
+a1['friend'] = "Javed"
+console.log(a1)
+
+
+// Question 05
+const dict = {
+    word: "meaning.",
+}
