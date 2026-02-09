18f0976ba51429abe4fda9006f26c2fa91645bc7
abdulrahmaannhh <abdulrahmaannhh@gmail.com>
2026-02-09 11:03:27 +0530

    Paractice set 2 question number 1 solved

---
+++ chapter_02_practice_set/script.js
@@ -0,0 +1,7 @@
+let age = prompt("Enter your age: ");
+if (age > 10 && age < 20) {
+  alert("Your age is " + age + " and its between 10 and 20");
+}
+else {
+    alert("Your age is " + age + "  and it is not between 10 and 20");
+}
