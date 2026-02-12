d7ceae3d5ef13f35c4976296135e9508675a2296
abdulrahmaannhh <abdulrahmaannhh@gmail.com>
2026-02-12 10:57:34 +0530

    Paractice set 2 question number 2 solved

---
+++ chapter_02_practice_set/Question_2/script.js
@@ -0,0 +1,17 @@
+let age = prompt("What is your age?")
+switch (age) {
+    case '12':
+        console.log("your age is 12")
+        break
+    case '13':
+        console.log("your age is 13")
+    case '14':
+        console.log("your age is 14")
+        break
+    case '15':
+        console.log("your age is 15")
+        break
+    default:
+        console.log("your age is not special")
+
+}
