fc81c92628dbfd975516b3010020e83fc5111f75
abdulrahmaannhh <abdulrahmaannhh@gmail.com>
2026-04-09 21:55:57 +0530

    Question 4 completed

---
+++ chapter_02_practice_set/Question_4/script.js
@@ -0,0 +1,15 @@
+let num = prompt("Enter a number: ");
+if (num % 2 === 0 && num % 3 === 0) {
+    console.log("The number is divisible by both 2 and 3.");
+}
+else if (num % 2 === 0) {
+    console.log("The number is divisible by 2.");
+}
+
+else if (num % 3 === 0) {
+    console.log("The number is divisible by 3.");
+}
+
+else {
+    console.log("The number is not divisible by 2 or 3.");
+}
